6db31b195faacfc8604e6d5585a60915cf9ded00
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-28 10:42:15 +0300

    Add files via upload

---
+++ 02. MaxMinNumberInRange/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numbers = Console.ReadLine().
+                Split(' ').
+                Select(int.Parse).
+                ToArray();
+
+int startIndex = int.Parse(Console.ReadLine());
+int endIndex = int.Parse(Console.ReadLine());
+
+int min  = numbers.Min();
+int max = numbers.Max();
+
+for (int i = startIndex; i < endIndex; i++) 
+{
+if (numbers[i] < min) 
+    {  
+        min = numbers[i]; 
+    }
+if (numbers[i] > max) 
+    {
+        max = numbers[i]; 
+    }
+}
+int sum = min + max;
+Console.WriteLine(sum);
