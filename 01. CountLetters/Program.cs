1d353268009a3a44e0cacbc1e9c2e61bbe8b1c4f
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-28 10:40:21 +0300

    Add files via upload

---
+++ 01. CountLetters/Program.cs
@@ -0,0 +1,24 @@
+﻿string text = Console.ReadLine();
+
+int upperCaseCount  = 0;
+int lowerCaseCount = 0;
+int spaceCount = 0;
+
+foreach(char ch in text) 
+{
+    if (char.IsUpper(ch)) 
+    {  
+        upperCaseCount++; 
+    }
+    else if (char.IsLower(ch)) 
+    { 
+        lowerCaseCount++; 
+    }
+    else if (char.IsWhiteSpace(ch)) 
+    {
+    spaceCount++;
+    }
+}
+Console.WriteLine(upperCaseCount);
+Console.WriteLine(lowerCaseCount);
+Console.WriteLine(spaceCount);
